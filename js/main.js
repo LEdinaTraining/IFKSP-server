e11e8d8a5f61a84c804af1bb456401fe627ef582
LEdinaTraining <nindzsaaa@gmail.com>
2020-08-11 23:04:43 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ function getServerdata(url) {
         cache: "no-cache"
     };
 
-     fetch(url, fetchOptions).then(
+     return fetch(url, fetchOptions).then(
         response => response.json(),
         err => console.error(err)
-    ).then(
-        data => console.log(data)
     );
 }
+getServerdata("http://localhost:3000/users").then(
+    data => console.log(data)
+);
