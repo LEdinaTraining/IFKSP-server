a7d074fcf20f2bda9b86235d1a10e70e5f79cf48
LEdinaTraining <nindzsaaa@gmail.com>
2020-08-12 01:14:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,11 +6,43 @@ function getServerdata(url) {
         cache: "no-cache"
     };
 
-     return fetch(url, fetchOptions).then(
+    return fetch(url, fetchOptions).then(
         response => response.json(),
         err => console.error(err)
     );
 }
-getServerdata("http://localhost:3000/users").then(
-    data => console.log(data)
-);
+
+document.querySelector("#getDataBtn").addEventListener("click", function () {
+    getServerdata("http://localhost:3000/users").then(
+        data => fillDataTable(data, "userTable")
+    );
+
+});
+
+// Fill table with server data.
+function fillDataTable(data, tableID) {
+    let table = document.querySelector(`#${tableID}`);
+    if (!table) {
+        console.error(`Table "${tableID}" is not found.`);
+        return;
+    }
+
+    let tBody = table.querySelector("tbody");
+    for (let row of data) {
+        let tr = createAnyElement("tr");
+        for (let k in row) {
+            let td = createAnyElement("td");
+            td.innerHTML = row[k];
+            tr.appendChild(td);
+        }
+        tBody.appendChild(tr);
+    }
+}
+
+function createAnyElement(name, attributes) {
+    let element = document.createElement(name);
+    for (let k in attributes) {
+        element.setAttribute(k, attributes[k]);
+    }
+    return element;
+}
